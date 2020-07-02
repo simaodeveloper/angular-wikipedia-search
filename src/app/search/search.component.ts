9b803b4218dcc2532183d8cb22fd8ebeefc4062e
Daniel Simão da Silva <simaodeveloper@gmail.com>
2020-07-02 14:29:58 -0300

    chore: style search component with bulma

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
   @Output() submitted = new EventEmitter<string>();
-  term = '';
+  public term: string = '';
 
   constructor() { }
 
