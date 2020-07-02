f8ab5b134d048a9318dfe5af153f008296325d2a
Daniel Simão da Silva <simaodeveloper@gmail.com>
2020-07-02 00:20:48 -0300

    chore: add pages component scaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule
