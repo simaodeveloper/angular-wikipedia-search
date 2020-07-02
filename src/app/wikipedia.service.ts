d527b4492acf76819d241f41084e43cd62982cee
Daniel Simão da Silva <simaodeveloper@gmail.com>
2020-07-02 01:55:15 -0300

    chore: add method search at wikipedia service

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -7,4 +7,18 @@ import { HttpClient } from '@angular/common/http'
 export class WikipediaService {
 
   constructor(private http: HttpClient) { }
+
+  search(term: string) {
+    return this.http
+      .get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          format: 'json',
+          list: 'search',
+          utf8: '1',
+          srsearch: term,
+          origin: '*'
+        }
+      });
+  }
 }
